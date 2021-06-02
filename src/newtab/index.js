8c60278064dd8519d2ec1663897fc2bad323a89c
Uday Hiwarale <thatisuday@gmail.com>
2021-06-02 23:40:41 +0200

    Update index.js

--- src/newtab/index.js
+++ src/newtab/index.js
@@ -8,7 +8,7 @@ chrome.storage.sync.get(['tabUrls', 'currentUrlIndex'], function(results) {
   var currentUrlIndex = results.currentUrlIndex;
 
   if (tabUrls[currentUrlIndex]) {
-    iframe.src = tabUrl;
+    iframe.src = tabUrls[currentUrlIndex];
     var nextTabIndex = currentUrlIndex < currentUrlIndex.length - 1 ? currentUrlIndex + 1 : 0;
     return chrome.storage.sync.set({ currentUrlIndex: nextTabIndex });
   }
