42573c157b5c11ccf5e92f0413ff8cbc66143c49
Uday Hiwarale <thatisuday@gmail.com>
2021-06-02 23:28:46 +0200

    Update index.js

--- src/action/index.js
+++ src/action/index.js
@@ -1,17 +1,26 @@
 document.getElementById('form').addEventListener('submit', function(e) {
   e.preventDefault();
-  var url = e.target.elements.url.value;
-  if (!url) {
+  var urlsFormValue = e.target.elements.url.value;
+  if (!urlsFormValue) {
     return;
   }
-
-  if (!/^https?:\/\//i.test(url)) {
-    url = 'https://' + url;
-  }
-  chrome.storage.sync.set({ tabUrl: url }, function() {
+  
+  // split URLs by `,`
+  var urls = urlsFormValue.split(',').map( url => {
+    if (!/^https?:\/\//i.test(url.trim())) {
+      return 'https://' + url.trim();
+    }
+    
+    return url.trim();
+  } );
+  
+  // store url
+  var urlsJson = JSON.stringify(urls);
+  
+  chrome.storage.sync.set({ tabUrls: urlsJson, currentUrlIndex: 0 }, function() {
     window.close();
     chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
-      chrome.tabs.update(tab.id, { url: url });
+      chrome.tabs.update(tab.id, { url: urls[0] });
     });
   });
 });
